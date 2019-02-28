809195682efb846a43fe52c7e4fc79f418c6b7d7
camposLuis <luis.campos.henrique@hotmail.com>
2019-02-28 00:12:11 -0300

    Stateful vs Stateless

--- aulas/modulo1/src/Button.js
+++ aulas/modulo1/src/Button.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-const Button = props => {
-  <button onClick={props.onClick}>{props.children}</button>;
-};
+const Button = props => (
+  <button onClick={props.onClick}>{props.children}</button>
+);
 
 Button.defaultProps = {
   children: "Somar"
