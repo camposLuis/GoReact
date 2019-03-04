38b011beea46729a049cecad9a047256811c6d04
camposLuis <luis.campos.henrique@hotmail.com>
2019-03-03 22:39:51 -0300

    Lista de todos

--- aulas/modulo3/src/App.js
+++ aulas/modulo3/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
+import TodoList from './TodoList';
 
 const App = () => (
   <Provider store={store}>
-    <h1>Hello World</h1>
+    <TodoList />
   </Provider>
 );
 
