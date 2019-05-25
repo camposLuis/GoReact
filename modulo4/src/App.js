910e3e5da196e48983210bcb2b300b2472e7269c
camposLuis <luis.campos.henrique@hotmail.com>
2019-05-25 16:06:19 -0300

    Organizando estrutura

---
+++ modulo4/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  </>
+);
+
+export default App;
