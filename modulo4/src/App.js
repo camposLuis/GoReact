0c1463422f4b64f28519163e1f0a6c0dd5f676eb
camposLuis <luis.campos.henrique@hotmail.com>
2019-05-26 16:04:59 -0300

    Estilizando Player

--- modulo4/src/App.js
+++ modulo4/src/App.js
@@ -3,11 +3,19 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 
 import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+
+import { Wrapper, Container } from './styles/components';
 
 const App = () => (
   <>
     <GlobalStyle />
-    <Sidebar />
+    <Wrapper>
+      <Container>
+        <Sidebar />
+      </Container>
+      <Player />
+    </Wrapper>
   </>
 );
 
