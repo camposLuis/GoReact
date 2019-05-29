2348da104402ef2837e5fa88920707b65d319299
camposLuis <luis.campos.henrique@hotmail.com>
2019-05-28 21:10:49 -0300

    Configurando Redux e Saga

---
+++ modulo4/src/store/index.js
@@ -0,0 +1,26 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import reducers from './ducks';
+import sagas from './sagas';
+
+const middlewares = [];
+
+const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+middlewares.push(sagaMiddleware);
+
+const createAppropriateStore = process.env.NODE_ENV === 'development' ? console.tron.createEnhancer : createStore;
+
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(...middlewares),
+    createAppropriateStore(...[]),
+  ),
+);
+
+sagaMiddleware.run(sagas);
+
+export default store;
